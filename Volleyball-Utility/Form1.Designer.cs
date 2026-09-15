0417f89c32d026f0f2a27fbcac17a0d4e394c81c
niall_croc <corkyn2005@gmail.com>
2026-09-15 21:26:17 +0100

    created form design and layout

--- Volleyball-Utility/Form1.Designer.cs
+++ Volleyball-Utility/Form1.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.NameInput = new System.Windows.Forms.TextBox();
+            this.AcceptName = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.emailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.courtScrambleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NameInput
+            // 
+            this.NameInput.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.NameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameInput.Location = new System.Drawing.Point(238, 233);
+            this.NameInput.Multiline = true;
+            this.NameInput.Name = "NameInput";
+            this.NameInput.Size = new System.Drawing.Size(326, 37);
+            this.NameInput.TabIndex = 0;
+            this.NameInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // AcceptName
+            // 
+            this.AcceptName.Location = new System.Drawing.Point(306, 276);
+            this.AcceptName.Name = "AcceptName";
+            this.AcceptName.Size = new System.Drawing.Size(194, 45);
+            this.AcceptName.TabIndex = 1;
+            this.AcceptName.Text = "Enter";
+            this.AcceptName.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.emailToolStripMenuItem,
+            this.courtScrambleToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // emailToolStripMenuItem
+            // 
+            this.emailToolStripMenuItem.Name = "emailToolStripMenuItem";
+            this.emailToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.emailToolStripMenuItem.Text = "Email";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(593, 73);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Attendance Tracker";
+            // 
+            // courtScrambleToolStripMenuItem
+            // 
+            this.courtScrambleToolStripMenuItem.Name = "courtScrambleToolStripMenuItem";
+            this.courtScrambleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.courtScrambleToolStripMenuItem.Text = "Court Scramble";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AcceptName);
+            this.Controls.Add(this.NameInput);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox NameInput;
+        private System.Windows.Forms.Button AcceptName;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem emailToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem courtScrambleToolStripMenuItem;
     }
 }
 
